9dee55e03047ae3042c41f62351c7530769625c3
Giridhar8888 <balinenigiridharnaidu@gmail.com>
2024-07-22 04:03:45 +0530

    djdjdjd

---
+++ GiridharProject/src/main/java/GiridharProject/GiridharProject/App.java
@@ -0,0 +1,13 @@
+package GiridharProject.GiridharProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
