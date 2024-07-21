710927cfe2e4116f7755b83e7e7b19f798025630
Giridhar8888 <balinenigiridharnaidu@gmail.com>
2024-07-21 23:39:54 +0530

    docker

---
+++ ThirdMavenProject/src/main/java/ThirdMavenProject/ThirdMavenProject/App.java
@@ -0,0 +1,13 @@
+package ThirdMavenProject.ThirdMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
